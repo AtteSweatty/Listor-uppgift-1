8511d87168cabcd9b763996be3822ab821258dfa
arha06001 <arha06001@E-R90YE0ET>
2024-02-02 13:19:28 +0100

    Add project files.

---
+++ Listor uppgift 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Atte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namnLista = new List<string>();
+
+            Console.WriteLine("Skriv in ditt namn");
+
+            while (Console.ReadLine() is { } namn && !string.IsNullOrEmpty(namn))
+            {
+                namnLista.Add(namn);
+            }
+
+            namnLista.Reverse();
+
+            Console.WriteLine("\nNamn i omvänd:");
+
+            foreach (var namn in namnLista)
+            {
+                Console.WriteLine(namn);
+            }
+        }
+    }
+}
